2927faef97393a243aaa5a4d29d0456a7f7fa3ca
William Roy <wroy@proton.me>
2022-07-21 14:58:15 -0400

    Add a basic post install script
    Signed-off-by: William Roy <wroy@proton.me>

---
+++ System/Fedora/postinstall.sh
@@ -0,0 +1,7 @@
+sudo dnf upgrade -y
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
+sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install -y lame\* --exclude=lame-devel
+sudo dnf group upgrade -y --with-optional Multimedia
